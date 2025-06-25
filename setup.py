4edbc430781f7cf33fa52903b76019827584fd73
Adam Twardoch <adam+github@twardoch.com>
2025-06-25 23:17:01 +0200

    Auto-merge: Resolved conflicts

---
+++ setup.py
@@ -0,0 +1,20 @@
+# +
+# Distutils script to install python_freetype. Invoke from the command line
+# in this directory as follows:
+#
+#     python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+# -
+
+import distutils.core
+
+distutils.core.setup(
+    name="Python FreeType",
+    version="0.6",
+    description="language bindings for the FreeType library, for Python 3.2 or later",
+    author="Lawrence D'Oliveiro",
+    author_email="ldo@geek-central.gen.nz",
+    url="http://github.com/ldo/python_freetype",
+    py_modules=["freetype2"],
+)
